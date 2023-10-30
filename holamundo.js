52e2c67256539471c8cec7b5ac857deb99faf96d
Jaime <jaime.menor.rios@gmail.com>
2023-10-30 13:09:38 +0100

    js files adadpted to node, holamundo and fibonacci (ab - amend)

--- holamundo.js
+++ holamundo.js
@@ -1,2 +1,11 @@
-let nombre = 'jaime'
-console.log ('Hola mundo, soy' + nombre);
+console.log("Programa para imprimir la clase");
+const name = "ASIR2"
+let number = 5;
+
+
+for (let i = 0; i < number; i++){
+	console.log(name);
+}
+
+
+
